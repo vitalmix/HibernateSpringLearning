92f92c154f646ac72612680407907dcc82631235
Vitalij Stinskij <vstinskij@gmail.com>
2021-05-31 16:49:50 +0300

    add retrieve object by PK using session.get

--- src/main/java/com/company/Run.java
+++ src/main/java/com/company/Run.java
@@ -5,10 +5,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Run {
     public static void main(String[] args) {
 
-        //21 - 1
+        //21 - 4
 
         SessionFactory factory = new Configuration()
                 .configure()
@@ -18,7 +20,7 @@ public class Run {
         Session session = factory.getCurrentSession();
 
         try {
-
+/*
             StarShip starShip =
                     new StarShip("A441", "LaserGun", "15", 0, true);
 
@@ -26,10 +28,27 @@ public class Run {
 
             session.save(starShip);
 
-            session.getTransaction().commit();
+            session.getTransaction().commit();*/
+
+            getShip(session,1);
 
         } finally {
             factory.close();
         }
     }
+
+
+    public static void getShip(Session session, int id) {
+
+        session.beginTransaction();
+
+        StarShip starShip = session.get(StarShip.class, id);
+
+        session.getTransaction().commit();
+
+        System.out.println("STAR SHIP IS READY! " + starShip.toString());
+
+
+    }
+
 }
